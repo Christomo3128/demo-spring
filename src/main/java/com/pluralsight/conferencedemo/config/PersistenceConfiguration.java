487fe760de3c4c4ee3675a32f9e0ab566cd60e0b
Christian Alberto Torres Moreno <ctorres@belatrix.com>
2020-03-11 16:04:24 -0500

    Proyecto enviado al repo local

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,26 @@
+package com.pluralsight.conferencedemo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceConfiguration {
+
+	@Value("${DB_URL}")
+	private String Urldb;
+	
+	@Bean
+	public DataSource dataSource() {
+		DataSourceBuilder builder = DataSourceBuilder.create();
+		builder.url(Urldb);
+		builder.username("postgres");
+		builder.password("admin");
+		System.out.println("Datasource personalizado esta funcionando.");
+		return builder.build();
+	}
+	
+}
